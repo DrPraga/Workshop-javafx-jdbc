f6cba300185c27ae31664141aab901fab0cbe022
DrPraga <drpragaofficial@gmail.com>
2021-05-17 10:13:01 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -2,23 +2,30 @@ package gui;
 
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentServices;
+
 
 public class DepartmentFormController implements Initializable{
 
 	private Department entitiy;
+	private DepartmentServices service;
 	
 	@FXML
 	private TextField txtId;
 	
 	@FXML
-	private TextField txtName;
+	private TextField txtName; 
 	
 	@FXML
 	private Label labelErrorName;
@@ -33,14 +40,40 @@ public class DepartmentFormController implements Initializable{
 		this.entitiy = entity;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentService(DepartmentServices service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onBtCancelAction() {
+	public void onBtSaveAction(ActionEvent event) {
+		if(entitiy == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entitiy = getFormData();
+			service.saveOrUpdate(entitiy);
+			gui.util.Utils.currenceStage(event);
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(gui.util.Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
 		System.out.println("onBtCancelAction");
+		gui.util.Utils.currenceStage(event);
 	}
 	
 	@Override
